ab94ca51995ac79aa03f61be1f919c7bb4d64f57
haku <git@haku.me>
2025-01-13 20:05:54 +0000

    Make TotalOverTime maybe threadsafe.  Microbenchmark suggests this has made calls to increment() ~20% slower.

--- src/main/java/com/vaguehope/common/util/TotalOverTime.java
+++ src/main/java/com/vaguehope/common/util/TotalOverTime.java
@@ -1,6 +1,8 @@
 package com.vaguehope.common.util;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicLongArray;
 
 import com.google.common.base.Ticker;
 
@@ -10,41 +12,42 @@ public class TotalOverTime {
 	private final long bucketDurationNanos;
 	private final Ticker ticker;
 
-	private final long[] ringBuffer;
+	private final AtomicLong total;
+	private final AtomicLongArray ringBuffer;
 	private volatile long lastIncrementNanos;
 
 	public TotalOverTime(final long duration, final TimeUnit timeUnit, final int bucketCount, final Ticker ticker) {
 		this.totalDurationNanos = timeUnit.toNanos(duration);
 		this.bucketDurationNanos = this.totalDurationNanos / bucketCount;
 		this.ticker = ticker;
-		this.ringBuffer = new long[bucketCount];
+		this.total = new AtomicLong();
+		this.ringBuffer = new AtomicLongArray(bucketCount);
 		this.lastIncrementNanos = ticker.read();
 	}
 
 	public void increment(final long delta) {
 		final long now = this.ticker.read();
 
-		final int expiredBucketCount = (int) ((now - this.lastIncrementNanos) / this.bucketDurationNanos);
+		final long lastIncrement = this.lastIncrementNanos;
+		final int expiredBucketCount = (int) ((now - lastIncrement) / this.bucketDurationNanos);
 		if (expiredBucketCount > 0) {
-			final int lastIncrementedBucket = (int) ((this.lastIncrementNanos % this.totalDurationNanos) / this.bucketDurationNanos);
+			final int lastIncrementedBucket = (int) ((lastIncrement % this.totalDurationNanos) / this.bucketDurationNanos);
 			for (int x = 1; x <= expiredBucketCount; x++) {
 				int i = lastIncrementedBucket + x;
-				if (i > this.ringBuffer.length - 1) i -= this.ringBuffer.length;
-				this.ringBuffer[i] = 0;
+				if (i > this.ringBuffer.length() - 1) i -= this.ringBuffer.length();
+				final long removed = this.ringBuffer.getAndSet(i, 0);
+				this.total.addAndGet(-removed);
 			}
 		}
 
 		final int currentBucket = (int) ((now % this.totalDurationNanos) / this.bucketDurationNanos);
-		this.ringBuffer[currentBucket] += delta;
+		this.ringBuffer.addAndGet(currentBucket, delta);
+		this.total.addAndGet(delta);
 		this.lastIncrementNanos = now;
 	}
 
 	public long get() {
-		long total = 0L;
-		for (int i = 0; i < this.ringBuffer.length; i++) {
-			total += this.ringBuffer[i];
-		}
-		return total;
+		return this.total.get();
 	}
 
 }
